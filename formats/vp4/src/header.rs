b334ec7077f42ada56e6c7bfdd0aeb1a6714f46a
Opal Symes <code@opal.codes>
2019-12-23 21:04:37 +1300

    Fixing `vp4`

--- formats/vp4/src/header.rs
+++ formats/vp4/src/header.rs
@@ -1,23 +1,17 @@
-
-
-use embroidery_lib::format::traits::PatternLoader;
 use embroidery_lib::prelude::*;
+use embroidery_lib::format::traits::PatternReader;
 use std::io::Read;
 
-const MAGIC_BYTES: [u8; 6]= [b'%', b'V', b'P', b'4', b'%', 0x01];
-
-
+const MAGIC_BYTES: [u8; 6] = [b'%', b'V', b'P', b'4', b'%', 0x01];
 
 #[derive(Default)]
-pub struct Vp4PatternLoader {}
+pub struct Vp4PatternReader {}
 
-impl PatternLoader for Vp4PatternLoader {
+impl PatternReader for Vp4PatternReader {
     fn is_loadable(&self, item: &mut dyn Read) -> Result<bool, ReadError> {
         let mut buf = [0u8; 6 /* MAGIC_BYTES.len() */];
         item.read_exact(&mut buf)?;
-        Ok(
-            buf == MAGIC_BYTES
-        )
+        Ok(buf == MAGIC_BYTES)
     }
 
     fn read_pattern(&self, item: &mut dyn Read) -> Result<Pattern, ReadError> {
