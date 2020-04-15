d5f2dd64b1ef39025c2f9c9af3fa9e103ba0446f
Opal Symes <code@opal.codes>
2020-04-15 21:33:17 +1200

    Split HUS & VIP format to prepare for writer

--- formats/hus/src/lib.rs
+++ formats/hus/src/lib.rs
@@ -1,28 +1,49 @@
 mod colors;
 mod header;
 mod read;
+mod write;
 
 use embroidery_lib::format::{PatternFormat, PatternReader, PatternWriter};
 
 pub use read::HusVipPatternReader;
+pub use write::HusVipPatternWriter;
 
-const NAME: &'static str = "hus/vip";
-const EXTENSIONS: [&'static str; 2] = ["hus", "vip"];
+const HUS_NAME: &'static str = "hus";
+const HUS_EXTENSIONS: [&'static str; 1] = ["hus"];
+const VIP_NAME: &'static str = "vip";
+const VIP_EXTENSIONS: [&'static str; 1] = ["vip"];
 
 #[derive(Default)]
-pub struct HusVipPatternFormat {}
+pub struct HusPatternFormat {}
 
-impl PatternFormat for HusVipPatternFormat {
+impl PatternFormat for HusPatternFormat {
     fn name<'a>(&self) -> &'a str {
-        NAME
+        HUS_NAME
     }
     fn extensions<'a, 'b>(&self) -> &'a [&'b str] {
-        &EXTENSIONS
+        &HUS_EXTENSIONS
     }
     fn reader(&self) -> Option<Box<dyn PatternReader>> {
         Some(Box::from(HusVipPatternReader::default()))
     }
     fn writer(&self) -> Option<Box<dyn PatternWriter>> {
-        None
+        Some(Box::from(HusVipPatternWriter::hus()))
+    }
+}
+#[derive(Default)]
+pub struct VipPatternFormat {}
+
+impl PatternFormat for VipPatternFormat {
+    fn name<'a>(&self) -> &'a str {
+        VIP_NAME
+    }
+    fn extensions<'a, 'b>(&self) -> &'a [&'b str] {
+        &VIP_EXTENSIONS
+    }
+    fn reader(&self) -> Option<Box<dyn PatternReader>> {
+        Some(Box::from(HusVipPatternReader::default()))
+    }
+    fn writer(&self) -> Option<Box<dyn PatternWriter>> {
+        Some(Box::from(HusVipPatternWriter::vip()))
     }
 }
--- src/formats.rs
+++ src/formats.rs
@@ -2,15 +2,16 @@ use embroidery_lib::format::{PatternFormat, PatternReader, PatternWriter};
 
 use embroidery_fmt_csv::CsvPatternFormat;
 use embroidery_fmt_dst::DstPatternFormat;
-use embroidery_fmt_hus::HusVipPatternFormat;
+use embroidery_fmt_hus::{HusPatternFormat, VipPatternFormat};
 use embroidery_fmt_svg::SvgPatternFormat;
 
 pub fn get_all() -> Vec<Box<dyn PatternFormat>> {
     vec![
         Box::new(CsvPatternFormat::default()),
         Box::new(DstPatternFormat::default()),
-        Box::new(HusVipPatternFormat::default()),
+        Box::new(HusPatternFormat::default()),
         Box::new(SvgPatternFormat::default()),
+        Box::new(VipPatternFormat::default()),
     ]
 }
 pub fn get_readers() -> Vec<Box<dyn PatternReader>> {
