3b02159dcfdbd1795db01fd5f930dcd0018ceff3
Opal Symes <code@opal.codes>
2020-04-15 21:26:47 +1200

    Combine VP3 & VF3

--- formats/vp3/src/lib.rs
+++ formats/vp3/src/lib.rs
@@ -1,28 +1,5 @@
-mod read;
-// mod write;
+mod vf3;
+mod vp3;
 
-use embroidery_lib::format::{PatternFormat, PatternReader, PatternWriter};
-
-pub use self::read::Vp3PatternReader;
-// pub use self::write::Vp3PatternWriter;
-
-const NAME: &'static str = "vp3";
-const EXTENSIONS: [&'static str; 1] = ["vp3"];
-
-#[derive(Default)]
-pub struct Vp3PatternFormat {}
-
-impl PatternFormat for Vp3PatternFormat {
-    fn name<'a>(&self) -> &'a str {
-        NAME
-    }
-    fn extensions<'a, 'b>(&self) -> &'a [&'b str] {
-        &EXTENSIONS
-    }
-    fn reader(&self) -> Option<Box<dyn PatternReader>> {
-        Some(Box::from(Vp3PatternReader::default()))
-    }
-    fn writer(&self) -> Option<Box<dyn PatternWriter>> {
-        None
-    }
-}
+pub use self::vf3::Vf3CollectionFormat;
+pub use self::vp3::{Vp3PatternFormat, Vp3PatternReader};
--- formats/vp3/tests/vp3.rs
+++ formats/vp3/tests/vp3.rs
@@ -5,7 +5,7 @@ use embroidery_fmt_vp3::Vp3PatternFormat;
 #[test]
 fn test_t160_vp3_file_read() {
     // Taken from http://en.embgallery.com/sites/default/files/freedesign/T160.zip
-    let mut data: &[u8] = include_bytes!("test_data/T160.vp3");
+    let mut data: &[u8] = include_bytes!("test_data/vp3/embgallery/T160.vp3");
     let loader = Vp3PatternFormat::default().reader().unwrap();
     loader.read_pattern(&mut data).unwrap();
 }
@@ -13,7 +13,7 @@ fn test_t160_vp3_file_read() {
 #[test]
 fn test_t42_1_vp3_file_read() {
     // Taken from http://en.embgallery.com/sites/default/files/freedesign/T42-1.zip
-    let mut data: &[u8] = include_bytes!("test_data/T42-1.vp3");
+    let mut data: &[u8] = include_bytes!("test_data/vp3/embgallery/T42-1.vp3");
     let loader = Vp3PatternFormat::default().reader().unwrap();
     loader.read_pattern(&mut data).unwrap();
 }
